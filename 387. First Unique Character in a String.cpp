be21c69513d69d17d6c47cd162745e8802c60ac0
Md Tanvir Hossain <md.tanvir.hossai.tusher>
2024-03-28 10:23:08 +0600

    387 added

--- 387. First Unique Character in a String.cpp
+++ 387. First Unique Character in a String.cpp
@@ -55,3 +55,25 @@ public:
         return -1;
     }
 };
+
+// TC = MC: O(n)
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        vector<int> v(26, 0);
+        for (auto c : s)
+        {
+            v[c - 'a']++;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (v[s[i] - 'a'] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
